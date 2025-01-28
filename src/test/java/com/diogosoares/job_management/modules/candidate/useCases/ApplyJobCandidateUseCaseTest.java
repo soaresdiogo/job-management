76e6bcf53d4af2079111c7d0c6bbd9b8478e5088
Diogo Soares <diogoschsoares@gmail.com>
2025-01-28 09:25:49 +0000

    Added tests

---
+++ src/test/java/com/diogosoares/job_management/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,72 @@
+package com.diogosoares.job_management.modules.candidate.useCases;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.diogosoares.job_management.application.exceptions.UserNotFoundException;
+import com.diogosoares.job_management.domain.modules.candidate.enterprise.entities.ApplyJobEntity;
+import com.diogosoares.job_management.domain.modules.candidate.enterprise.entities.CandidateEntity;
+import com.diogosoares.job_management.domain.modules.candidate.useCases.ApplyJobCandidateUseCase;
+import com.diogosoares.job_management.domain.modules.company.enterprise.entities.JobEntity;
+import com.diogosoares.job_management.infrastructure.modules.candidate.repositories.ApplyJobRepository;
+import com.diogosoares.job_management.infrastructure.modules.candidate.repositories.CandidateRepository;
+import com.diogosoares.job_management.infrastructure.modules.company.repositories.JobRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Mock
+    private ApplyJobRepository applyJobRepository;
+
+    @Test
+    @DisplayName("Should not be able to apply for a job if candidate does not found")
+    public void should_not_be_apply_jon_with_candidate_not_found() {
+        try{
+            applyJobCandidateUseCase.execute(null, null);
+        } catch (Exception e){
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    public void should_be_able_to_create_a_new_apply_job() {
+        var idCandidate = UUID.randomUUID();
+        var idJob = UUID.randomUUID();
+
+        var applyJob = ApplyJobEntity.builder().candidateId(idCandidate)
+                .jobId(idJob).build();
+
+        var applyJobCreated = ApplyJobEntity.builder().id(UUID.randomUUID()).build();
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(new CandidateEntity()));
+        when(jobRepository.findById(idJob)).thenReturn(Optional.of(new JobEntity()));
+
+        when(applyJobRepository.save(applyJob)).thenReturn(applyJobCreated);
+
+        var result = applyJobCandidateUseCase.execute(idCandidate, idJob);
+
+        assertThat(result).hasFieldOrProperty("id");
+        assertNotNull(result.getId());
+
+    }
+}
---
+++ src/test/java/com/diogosoares/job_management/modules/company/controllers/CreateJobControllerTest.java
@@ -0,0 +1,88 @@
+package com.diogosoares.job_management.modules.company.controllers;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.diogosoares.job_management.domain.modules.company.enterprise.entities.CompanyEntity;
+import com.diogosoares.job_management.infrastructure.modules.company.dto.CreateJobDTO;
+import com.diogosoares.job_management.infrastructure.modules.company.repositories.CompanyRepository;
+import com.diogosoares.job_management.utils.TestUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class CreateJobControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @BeforeEach
+    public void setup(){
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(SecurityMockMvcConfigurers.springSecurity())
+            .build();
+    }
+
+    @Test
+    public void should_be_able_to_create_a_new_job() throws Exception{
+
+        var company = CompanyEntity.builder()
+            .description("COMPANY_DESCRIPTION")
+            .email("email@company.com")
+            .password("1234567890")
+            .username("COMPANY_USERNAME")
+            .name("COMPANY_NAME").build();
+
+        company = companyRepository.saveAndFlush(company);
+
+        var createdJobDTO = CreateJobDTO.builder()
+        .benefits("BENEFITS_TEST")
+        .description("DESCRIPTION_TEST")
+        .level("LEVEL_TEST")
+        .build();
+
+        var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtils.objectToJson(createdJobDTO))
+            .header("Authorization", TestUtils.generateToken(company.getId(), "JAVAJOBS_@123#"))
+            )
+            .andExpect(MockMvcResultMatchers.status().isOk());
+
+        System.out.println(result);
+    }
+
+    @Test
+    public void should_not_be_able_to_create_a_new_job_if_company_not_found() throws Exception{
+        var createdJobDTO = CreateJobDTO.builder()
+        .benefits("BENEFITS_TEST")
+        .description("DESCRIPTION_TEST")
+        .level("LEVEL_TEST")
+        .build();
+
+        mvc.perform(MockMvcRequestBuilders.post("/company/job/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtils.objectToJson(createdJobDTO))
+            .header("Authorization", TestUtils.generateToken(UUID.randomUUID(), "JAVAJOBS_@123#")))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+}
